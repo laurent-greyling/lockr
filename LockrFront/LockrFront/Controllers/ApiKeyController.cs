a200fc15c746fe3f7023aa5dbd5189d3a745427e
laurent.greyling@gmail.com <laurent.greyling@gmail.com>
2020-01-06 22:07:46 +0100

    basic setup for api key stuff

---
+++ LockrFront/LockrFront/Controllers/ApiKeyController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LockrFront.Controllers
+{
+    //Controller to generate and save the apikey 
+    public class ApiKeyController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Logic to create, save and returnapi key
+            return View();
+        }
+    }
+}
